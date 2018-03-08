2b69ede9b81f0bcbb536dd0a31d116ffc9559a80
“july_haidingPC <july95685@gmail.com>
2018-03-08 21:37:01 +0800

    no message

---
+++ js打包/app.js
@@ -0,0 +1,5 @@
+import sum from './sum'
+
+var minus = require('./minus.js')
+console.log('sum(23,24) = ' + sum(23,24));
+console.log('minus(25,24) = ' + minus(25,24));
---
+++ js打包/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	entry: {
+		app:'./app.js'
+	},
+	output: {
+		filename: 'bundle.[hash:5].js'
+	}
+}
